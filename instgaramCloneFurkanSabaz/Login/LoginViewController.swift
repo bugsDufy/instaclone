67390f6652246dfe8c605d4b5edddd692aa2bb10
bugsDufy <mrtgules@gmail.com>
2023-12-17 14:49:11 +0300

    login page to add

---
+++ instgaramCloneFurkanSabaz/Login/LoginViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LoginViewController.swift
+//  instgaramCloneFurkanSabaz
+//
+//  Created by Murat Gules on 17.12.2023.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+    }
+    
+
+    
+
+}
---
+++ instgaramCloneFurkanSabaz/Login/LoginViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  LoginViewModel.swift
+//  instgaramCloneFurkanSabaz
+//
+//  Created by Murat Gules on 17.12.2023.
+//
+
+import UIKit
+
+class LoginViewModel {
+    
+    struct LoginView {
+        
+    }
+}
